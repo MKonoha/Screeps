a676051aa6554a4236b98d6c8b0b57d20872777a
Patrick Carton <pcarton927@gmail.com>
2018-09-30 12:42:38 -0400

    added dequeue and finding source ids

--- module.memory.js
+++ module.memory.js
@@ -12,15 +12,28 @@ var memoryHandler = {
     Memory.rooms[roomName] = {
       spawnQ: [],
       jobQ: [],
-      sourceIDs: [],
+      sources: getSourceIds(roomName),
+      controller: Game.rooms[roomName].conroller.id,
       signMsg: "",
       initialized: false, //Set to true when init is run
     };
     Memory.rooms[roomName].initialized = true;
   },
 
+  getSourceIds(roomName){
+    var ids = [];
+    const sources = Game.rooms[roomName].find(FIND_SOURCES);
+    for(var source in sources){
+      ids.push(source.id);
+    }
+  },
+
   storeCreepsJob(creep, job){
     creep.memory.job = job;
+  },
+
+  dequeueJob(roomName){
+    Memory.rooms[roomName].jobQ.shift(job);
   },
 
   enqueueJob(roomName,job,priority){
