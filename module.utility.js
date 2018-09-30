7f0a6278c9209bb85609097fd8e896b5c11ccda9
Patrick Carton <pcarton927@gmail.com>
2018-09-30 12:42:57 -0400

    added adding job to creep if not assigned

--- module.utility.js
+++ module.utility.js
@@ -1,10 +1,14 @@
-var jobManager = require('./module.jobManager.js');
+var jobManager = require('module.jobManager');
+var memoryModule = require('module.memory');
 var utilityModule = {
 
   runCreep: function(creep){
     var job = creep.memory.job;
     if(job){
       jobManager.doJob(creep,job);
+    }else{
+      const newJob = memoryModule.dequeueJob(creep.room.name);
+      memoryModule.storeCreepJob(creep, newJob);
     }
   },
 
